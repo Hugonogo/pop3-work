42b2f3dc2b4183a68216d1fdf6fa4db95ce6fd9a
UmJovemProgramador <112925082+UmJovemProgramador@users.noreply.github.com>
2022-10-24 17:34:54 -0300

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,69 +1,57 @@
-import java.util.ArrayList;
-import java.util.List;
-public class Server {
-    public String usuario = "aluno";
-    public String senha = "123456";
-    public String endereco = "pop.mail.com";
-    public String porta = "110";
+public class Email {
+    private String remetente, destinatario, assunto, mensagem, tamanho;
 
-
-    //emails
-    List<Email> emails = new ArrayList<>(); //arraylist de emails do servidor
-
-
-    public void carregarEmails(){
-            emails.add(new Email("Lucas", usuario, "bom dia","\n Olá, tudo bem.", "1009" ));
-        emails.add(new Email("ShopOnline", usuario, "Promoção do shopOnline", "\nOlá, aluno\nA ShopOnline está com ótimas promoções\nvenha conferir!!!", "3553"));
-    emails.add(new Email("ClaroServiços@mail.com", usuario," Pacote Claro","\nOlá, Boa tarde, aluno\nGostaria do de comprar o novo pacote da claro?\nNosas promoções:\n3G R$20,00 ao mês\n4G R$40,00 ao mês\n5G 60,00 ao mês", "5799"));
-    }
-    public void caixaDeEntrada(int n){
-        System.out.println(emails.get(n).toString());
-        emails.remove(n);
-    }
-    //Comando de login
-    public String user(Client pass){
-
-        if (pass.getUser().equals(this.usuario)) {
-            return "+OK usário reconhecido";
-        }else{
-            return "-ERR usuário não reconhecido";
-        }
-        
-    }
-    public boolean pass(Client Pass){
-        if (Pass.getPassword().equals(this.senha)) {
-            return true;
-        }else{
-            return false;
-        }
-       
-    }
-    // Mostra o número de emails dp usuário
-    public int stat(){
-        int n = emails.size();
-        return n;
-    }
-    //Recebe como parâmetro o indice do email e a quantidade de linhas que deveraão ser mostradas
-    public void retr(int n){
-        if (n < 0 | n > emails.size()) {
-            System.out.println("-Erro- Impossível acessar essa informação\ne-mails não lidos = "
-                    + emails.size()+
-                    "\ntentou acessar a posição "+n+" da caixa postal");
-        }else{
-            caixaDeEntrada((n-1));
-        }
+    public String getTamanho() {
+        return tamanho;
     }
 
-    public void list(){
-
-        for (int i = 0; i < emails.size(); i++) {
-          
-            System.out.println(""+ (i+1)+" - "+emails.get(i).getTamanho() + "KB");//Imprime o indice dos emails e seu tamanho em KB, colocar o tamanho no final
-
-        }
-        System.out.println(".");
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
     }
-   
 
+    public Email(String remetente, String destinatario, String assunto, String mensagem, String tamanho) {
+        this.remetente = remetente;
+        this.destinatario = destinatario;
+        this.assunto = assunto;
+        this.mensagem = mensagem;
+        this.tamanho = tamanho;
+    }
+    
+    public String getRemetente() {
+        return remetente;
+    }
 
+    public void setRemetente(String remetente) {
+        this.remetente = remetente;
+    }
+
+    public String getDestinatario() {
+        return destinatario;
+    }
+
+    public void setDestinatario(String destinatario) {
+        this.destinatario = destinatario;
+    }
+
+    public String getAssunto() {
+        return assunto;
+    }
+
+    public void setAssunto(String assunto) {
+        this.assunto = assunto;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+    @Override
+    public String toString() {
+        return "\nDe: "+remetente+"// Para: "+destinatario+"\n Assunto"+assunto+"\n"+mensagem+"\n.";
+    }
+    
 }
+
