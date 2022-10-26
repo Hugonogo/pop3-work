2450b0856bec8b1069f1ed036c577ef217ece06a
Hugo <112352124+Hugonogo@users.noreply.github.com>
2022-10-25 21:16:46 -0300

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -13,9 +13,36 @@ public class Client {
     public void amazenarMailDoServe(List<Email> mailServeList){
         for (Email e : mailServeList){
             caixaPostal.add(e);
+            
         }
     }
-    
+    public void caixaPostal(int n){
+        System.out.println(caixaPostal.get(n).toString());
+        
+    }
+    public int stat(){
+        int n = caixaPostal.size();
+        return n;
+    }
+    public void retr(int n){
+        if (n < 0 | n > caixaPostal.size()) {
+            System.out.println("-Erro- Impossível acessar essa informação\ne-mails não lidos = "
+                    + caixaPostal.size()+
+                    "\ntentou acessar a posição "+n+" da caixa postal");
+        }else{
+            n--;
+           caixaPostal(n);
+        }
+    }
+    public void list(){
+
+        for (int i = 0; i < caixaPostal.size(); i++) {
+          
+            System.out.println(""+ (i+1)+" - "+caixaPostal.get(i).getTamanho() + "KB");//Imprime o indice dos emails e seu tamanho em KB, colocar o tamanho no final
+
+        }
+        System.out.println(".");
+    }
     public Client(String user, String password) {
         this.user = user;
         this.password = password;
