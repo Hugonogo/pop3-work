f977dc8af2e9e67869c5137e7eb9d99f8e82a812
Hugo <112352124+Hugonogo@users.noreply.github.com>
2022-11-01 21:38:23 -0300

    Update Email.java

--- model/Email.java
+++ model/Email.java
@@ -3,22 +3,23 @@ package model;
 import java.io.Serializable;
 
 public class Email implements Serializable{
-    /*
+     /*
      * Template do e-mail básico pode add mais coisa se quiser
     */
 
-    String tittle, msg, to, sender;
+    String tittle, msg, to, sender, tamanho;
 
-    public Email(String tittle, String msg, String to, String sender) {
+    public Email(String tittle, String msg, String to, String sender, String tamanho) {
         this.tittle = tittle;
         this.msg = msg;
         this.to = to;
         this.sender = sender;
+        this.tamanho = tamanho;
     }
 
     @Override
     public String toString() {
-        return "Email [tittle=" + tittle + ", msg=" + msg + ", to=" + to + ", sender=" + sender + "]";
+        return "\nDe: "+sender+"// Para: "+to+"\n Assunto: "+tittle+"\n"+msg+"\n.";
     }
 
 }
