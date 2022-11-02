7564d0a76b061c46b8f362d4604d20cdf9f2d050
Hugo <112352124+Hugonogo@users.noreply.github.com>
2022-11-02 19:11:13 -0300

    Update ParteClient.java

--- cliente/ParteClient.java
+++ cliente/ParteClient.java
@@ -1,5 +1,5 @@
 package cliente;
-import java.util.*;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.PrintStream;
@@ -11,69 +11,76 @@ import model.Email;
 
 public class ParteClient {
     /* Variáveis que vou receber do serve */
-    static ObjectInputStream in;
-    static DataInputStream inmsg;
-    static List<Email> caixaPostalLocal;
-    static boolean isLogado = false; // padrão false até o server dizer que está
-
-
-    static Socket cliente;
-
-    private static void  initCliente(){
-
-        try {
-
-            cliente = new Socket("0.0.0.0", 110);
-            inmsg = new DataInputStream(cliente.getInputStream());
-            if (inmsg.readBoolean() == true) {
-                System.out.println("Flamengo");
-            }
-
-
-            //isLogado = in.readBoolean();
-
-        }catch (IOException ex){
-            System.out.println(ex);
-        }
-    }
-
-    public static void main(String[] args) throws IOException, ClassNotFoundException
-    {
-        initCliente();
-        
-       /* Entrada do usuario */
-       while(true)
-       {
-
-        String entrada = new Scanner(System.in).nextLine().toString();
-        if(true && entrada.equals("list"))
-        {
-            int cont = 1;
-            for(Email e : caixaPostalLocal)
-                {
-                    System.out.println(cont+" "+e.tamanho);
-                    cont++;
-                }
-            System.out.println(".");
-        }
-        if (true && entrada.equals("xxx")) {
-            in = new ObjectInputStream(cliente.getInputStream());
-
-            caixaPostalLocal = (ArrayList<Email>) in.readObject();
-        }
-        
-        try 
-          {
-              PrintStream saida = new PrintStream(cliente.getOutputStream());
-              saida.println(entrada);
-          } 
-          catch (IOException ex) 
-          {
-              System.out.println(ex);
-          }
-       }
-    
-    }
-    
+   static ObjectInputStream in;
+   static DataInputStream inmsg;
+   static List<Email> caixaPostalLocal;
+   static boolean isLogado = false; // padrão false até o server dizer que está 
    
+
+   static Socket cliente;
+
+   private static void  initCliente(){
+
+       try {
+
+           cliente = new Socket("0.0.0.0", 110);
+           
+
+
+           //isLogado = in.readBoolean();
+
+       }catch (IOException ex){
+           System.out.println(ex);
+       }
+   }
+
+   public static void main(String[] args) throws IOException, ClassNotFoundException
+   {
+       initCliente();
+       
+      /* Entrada do usuario */
+      while(true)
+      {
+
+       String entrada = new Scanner(System.in).nextLine().toString();
+       String[] command = entrada.split(" ");
+       if (command[0].equals("pass")) {
+        inmsg = new DataInputStream(cliente.getInputStream());
+        if (inmsg.readBoolean() == true) {
+            isLogado = true;
+            
+        }
+       }
+       if (isLogado) {
+             if(entrada.equals("list"))
+            {
+                int cont = 1;
+                for(Email e : caixaPostalLocal)
+                    {
+                        System.out.println(cont+" "+e.tamanho);
+                        cont++;
+                    }
+                System.out.println(".");
+            }
+            if (entrada.equals("xxx")) {
+                in = new ObjectInputStream(cliente.getInputStream());
+
+                caixaPostalLocal = (ArrayList<Email>) in.readObject();
+            }
+       }
+      
+       
+       try 
+         {
+             PrintStream saida = new PrintStream(cliente.getOutputStream());
+             saida.println(entrada);
+         } 
+         catch (IOException ex) 
+         {
+             System.out.println(ex);
+         }
+      }
+   
+   }
+    
 }
