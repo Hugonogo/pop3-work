b75d2f8db5580e64d0dae018e9f8ff1f825f884a
gustavodias <gustavo024dias@gmail.com>
2022-11-04 12:30:01 -0300

    comando telnet adicionado

--- cliente/ParteClient.java
+++ cliente/ParteClient.java
@@ -13,6 +13,7 @@ import java.util.logging.Logger;
 import model.Email;
 
 public class ParteClient {
+
     /* Variáveis que vou receber do serve */
     static ObjectInputStream in;
     static DataInputStream inmsg;
@@ -34,95 +35,131 @@ public class ParteClient {
  
     public static void main(String[] args) throws IOException, ClassNotFoundException
     {
-        initCliente();
-         System.out.println("Faça Login Para acessar os emails!\nPrimeiro insira o user e em seguida sua sennha!");
-        /*Pega a caixa postal do cliente antes de logar mas não exibe */
- 
-        in = new ObjectInputStream(cliente.getInputStream());
-        caixaPostalLocal = (ArrayList<Email>) in.readObject();
         
-       /* Entrada do usuario */
-       while(true)
-       {
- 
-         String entrada = new Scanner(System.in).nextLine().toString();
-         String[] command = entrada.split(" ");
-         if (!session) {
-            if (!isLogado) {
-                if (isUser) {
-                    if (command[0].equals("pass") && command[1].equals("123@45")) 
-                    {
-                        inmsg = new DataInputStream(cliente.getInputStream());
-                            if (inmsg.readBoolean() == true) 
-                            {
-                                isLogado = true;
-                                System.out.println("Logado com sucesso");
-                                session  = true;
+        Scanner firstCommand = new Scanner(System.in);
+
+        while(true)
+        {
+            /* Comando telnet para fazer a conexão com o servidor  */
+            String[] commandf = firstCommand.nextLine().split(" ");
+
+            if( commandf.length > 2)
+            {   
+                if (commandf[0].equals("telnet"))
+                {   /* valida o host corretamente */
+                    if(commandf[1].equals("0.0.0.0"))
+                    {   /* valida a porta correta */
+                        if (commandf[2].equals("1100"))
+                        {   /* Se a porta e o host estiverem corretos inicia a aplicação do cliente */
+                            System.out.println("\n\n+OK pronto\n\n");
+
+                            initCliente();
+                            System.out.println("Bem-vindo\nFaça Login Para acessar os emails!\nPrimeiro insira o user e em seguida sua sennha!");
+                        /*Pega a caixa postal do cliente antes de logar mas não exibe */
+
+                        in = new ObjectInputStream(cliente.getInputStream());
+                        caixaPostalLocal = (ArrayList<Email>) in.readObject();
+                        
+                        /* Entrada do usuario */
+                        while(true)
+                        {
+
+                            String entrada = new Scanner(System.in).nextLine().toString();
+                            String[] command = entrada.split(" ");
+                            if (!session) {
+                            if (!isLogado) {
+                                if (isUser) {
+                                    if (command[0].equals("pass") && command[1].equals("123@45")) 
+                                    {
+                                        inmsg = new DataInputStream(cliente.getInputStream());
+                                            if (inmsg.readBoolean() == true) 
+                                            {
+                                                isLogado = true;
+                                                System.out.println("Logado com sucesso");
+                                                session  = true;
+                                            }
+                                    }else System.out.println("ERRO");
+                                }
                             }
-                    }else System.out.println("ERRO");
+                            }
+                            if (!session) {
+                                if (!isLogado) {
+                                if (!isUser) {
+                                    if (command[0].equals("user") && command[1].equals("aluno")) {
+                                        System.out.println("user Ok");
+                                        isUser = true;
+                                    }else System.out.println("ERRO");
+                                }
+                                    
+                                }
+                    
+                            }
+                            
+                            if (command[0].equals("quit")) {
+                            System.out.println("Bye");
+                            break;
+                            }
+                            
+                            if(isLogado && entrada.equals("list"))
+                            {
+
+                                int cont = 1;
+                                for(Email e : caixaPostalLocal)
+                                    {
+                                        System.out.println(cont+" "+e.getTamanho());
+                                        cont++;
+                                    }
+                                System.out.println(".");
+                            }
+                            if (isLogado && command[0].equals("retr") && command.length > 1) {
+                                try
+                                {
+                                    int n = Integer.parseInt(command[1]);
+                                    n--;
+                                    if (n < caixaPostalLocal.size()) {
+                                        System.out.println(caixaPostalLocal.get(n).toString());    
+
+                                    }else{
+                                        n++;
+                                        System.out.println("Email "+n+" não existe");
+                                    }
+                                }catch(Exception e)
+                                {
+                                    System.out.println("Argumento inválido!");
+                                }
+                            
+                                
+                                
+                            }
+                        
+                        try 
+                            {
+                                PrintStream saida = new PrintStream(cliente.getOutputStream());
+                                saida.println(entrada);
+                            } 
+                            catch (IOException ex) 
+                            {
+                                System.out.println(ex);
+                            }
+                        }
+                        }
+                        System.out.println("Porta inválida!");
+                    }
+                    else
+                    {
+                        System.out.println("Host inválido!");
+                    }
+                }
+                else
+                {
+                    System.out.println("Comando inválido ou indisponível no momento!");
                 }
             }
-         }
-         if (!session) {
-             if (!isLogado) {
-                if (!isUser) {
-                    if (command[0].equals("user") && command[1].equals("aluno")) {
-                        System.out.println("user Ok");
-                        isUser = true;
-                    }else System.out.println("ERRO");
-                }
-                 
-             }
-    
-         }
-         
-         if (command[0].equals("quit")) {
-            System.out.println("Bye");
-            break;
-         }
-         
-         if(isLogado && entrada.equals("list"))
-         {
- 
-             int cont = 1;
-             for(Email e : caixaPostalLocal)
-                 {
-                     System.out.println(cont+" "+e.getTamanho());
-                     cont++;
-                 }
-             System.out.println(".");
-         }
-         if (isLogado && command[0].equals("retr") && command.length > 1) {
-             try
-             {
-                 int n = Integer.parseInt(command[1]);
-                 n--;
-                 if (n < caixaPostalLocal.size()) {
-                     System.out.println(caixaPostalLocal.get(n).toString());    
- 
-                 }else{
-                     n++;
-                     System.out.println("Email "+n+" não existe");
-                 }
-             }catch(Exception e)
-             {
-                 System.out.println("Argumento inválido!");
-             }
-         
-             
-             
-         }
-     
-        try 
-          {
-              PrintStream saida = new PrintStream(cliente.getOutputStream());
-              saida.println(entrada);
-          } 
-          catch (IOException ex) 
-          {
-              System.out.println(ex);
-          }
-       }
+            else
+            {
+                System.out.println("Inicie com o comando telnet corretamente!");
+            }
+        }
     
     }
 }
