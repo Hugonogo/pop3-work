5a98bb556c6063dc706c65aa4b8580de6cfd0ce6
Hugo <112352124+Hugonogo@users.noreply.github.com>
2022-11-04 10:33:10 -0300

    Update ParteClient.java

--- cliente/ParteClient.java
+++ cliente/ParteClient.java
@@ -14,98 +14,115 @@ import model.Email;
 
 public class ParteClient {
     /* Variáveis que vou receber do serve */
-   static ObjectInputStream in;
-   static DataInputStream inmsg;
-   static List<Email> caixaPostalLocal;
-   static boolean isLogado = false; // padrão false até o server dizer que está 
-   static boolean session = false;
-   
-
-   static Socket cliente;
-
-   private static void  initCliente(){
-
-       try {
-           cliente = new Socket("0.0.0.0", 1100);
-       }catch (IOException ex){
-           System.out.println(ex);
-       }
-   }
-
-   public static void main(String[] args) throws IOException, ClassNotFoundException
-   {
-       initCliente();
-        System.out.println("Faça Login Para acessar os emails!");
-       /*Pega a caixa postal do cliente antes de logar mas não exibe */
-
-       in = new ObjectInputStream(cliente.getInputStream());
-       caixaPostalLocal = (ArrayList<Email>) in.readObject();
-       
-      /* Entrada do usuario */
-      while(true)
-      {
-
-        String entrada = new Scanner(System.in).nextLine().toString();
-        String[] command = entrada.split(" ");
-        if (!session) {
-            if (!isLogado) {
-                if (command[0].equals("pass")) 
-                {
-                    inmsg = new DataInputStream(cliente.getInputStream());
-                        if (inmsg.readBoolean() == true) 
-                        {
-                            isLogado = true;
-                            System.out.println("Logado com sucesso");
-                            session  = true;
-                        }
-                }
-            }
-                
-            
+    static ObjectInputStream in;
+    static DataInputStream inmsg;
+    static List<Email> caixaPostalLocal;
+    static boolean isLogado = false; // padrão false até o server dizer que está 
+    static boolean session = false;
+    static boolean isUser = false;
+ 
+    static Socket cliente;
+ 
+    private static void  initCliente(){
+ 
+        try {
+            cliente = new Socket("0.0.0.0", 1100);
+        }catch (IOException ex){
+            System.out.println(ex);
         }
+    }
+ 
+    public static void main(String[] args) throws IOException, ClassNotFoundException
+    {
+        initCliente();
+         System.out.println("Faça Login Para acessar os emails!\nPrimeiro insira o user e em seguida sua sennha!");
+        /*Pega a caixa postal do cliente antes de logar mas não exibe */
+ 
+        in = new ObjectInputStream(cliente.getInputStream());
+        caixaPostalLocal = (ArrayList<Email>) in.readObject();
         
-        if(isLogado && entrada.equals("list"))
-        {
-
-            int cont = 1;
-            for(Email e : caixaPostalLocal)
-                {
-                    System.out.println(cont+" "+e.getTamanho());
-                    cont++;
+       /* Entrada do usuario */
+       while(true)
+       {
+ 
+         String entrada = new Scanner(System.in).nextLine().toString();
+         String[] command = entrada.split(" ");
+         if (!session) {
+            if (!isLogado) {
+                if (isUser) {
+                    if (command[0].equals("pass") && command[1].equals("123@45")) 
+                    {
+                        inmsg = new DataInputStream(cliente.getInputStream());
+                            if (inmsg.readBoolean() == true) 
+                            {
+                                isLogado = true;
+                                System.out.println("Logado com sucesso");
+                                session  = true;
+                            }
+                    }else System.out.println("ERRO");
                 }
-            System.out.println(".");
-        }
-        if (isLogado && command[0].equals("retr") && command.length > 1) {
-            try
-            {
-                int n = Integer.parseInt(command[1]);
-                n--;
-                if (n < caixaPostalLocal.size()) {
-                    System.out.println(caixaPostalLocal.get(n).toString());    
-
-                }else{
-                    n++;
-                    System.out.println("Email "+n+" não existe");
-                }
-            }catch(Exception e)
-            {
-                System.out.println("Argumento inválido!");
             }
-
-            
-            
-        }
-    
-       try 
-         {
-             PrintStream saida = new PrintStream(cliente.getOutputStream());
-             saida.println(entrada);
-         } 
-         catch (IOException ex) 
-         {
-             System.out.println(ex);
          }
-      }
-   
-   }
+         if (!session) {
+             if (!isLogado) {
+                if (!isUser) {
+                    if (command[0].equals("user") && command[1].equals("aluno")) {
+                        System.out.println("user Ok");
+                        isUser = true;
+                    }else System.out.println("ERRO");
+                }
+                 
+             }
+    
+         }
+         
+         if (command[0].equals("quit")) {
+            System.out.println("Bye");
+            break;
+         }
+         
+         if(isLogado && entrada.equals("list"))
+         {
+ 
+             int cont = 1;
+             for(Email e : caixaPostalLocal)
+                 {
+                     System.out.println(cont+" "+e.getTamanho());
+                     cont++;
+                 }
+             System.out.println(".");
+         }
+         if (isLogado && command[0].equals("retr") && command.length > 1) {
+             try
+             {
+                 int n = Integer.parseInt(command[1]);
+                 n--;
+                 if (n < caixaPostalLocal.size()) {
+                     System.out.println(caixaPostalLocal.get(n).toString());    
+ 
+                 }else{
+                     n++;
+                     System.out.println("Email "+n+" não existe");
+                 }
+             }catch(Exception e)
+             {
+                 System.out.println("Argumento inválido!");
+             }
+         
+             
+             
+         }
+     
+        try 
+          {
+              PrintStream saida = new PrintStream(cliente.getOutputStream());
+              saida.println(entrada);
+          } 
+          catch (IOException ex) 
+          {
+              System.out.println(ex);
+          }
+       }
+    
+    }
 }
