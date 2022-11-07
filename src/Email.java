c21abd3ad5df897e7479aeca272e4d81bb2a5c5d
Hugo <112352124+Hugonogo@users.noreply.github.com>
2022-11-07 08:16:37 -0300

    Update Email.java

--- src/Email.java
+++ src/Email.java
@@ -1,57 +1,30 @@
-public class Email {
-    private String remetente, destinatario, assunto, mensagem, tamanho;
+package model;
 
-    public String getTamanho() {
-        return tamanho;
-    }
+import java.io.Serializable;
 
-    public void setTamanho(String tamanho) {
+public class Email implements Serializable{
+     /*
+     * Template do e-mail básico pode add mais coisa se quiser
+    */
+
+    String tittle, msg, to, sender, tamanho;
+
+    public Email(String tittle, String msg, String to, String sender, String tamanho) {
+        this.tittle = tittle;
+        this.msg = msg;
+        this.to = to;
+        this.sender = sender;
         this.tamanho = tamanho;
     }
 
-    public Email(String remetente, String destinatario, String assunto, String mensagem, String tamanho) {
-        this.remetente = remetente;
-        this.destinatario = destinatario;
-        this.assunto = assunto;
-        this.mensagem = mensagem;
-        this.tamanho = tamanho;
-    }
-    
-    public String getRemetente() {
-        return remetente;
-    }
-
-    public void setRemetente(String remetente) {
-        this.remetente = remetente;
-    }
-
-    public String getDestinatario() {
-        return destinatario;
-    }
-
-    public void setDestinatario(String destinatario) {
-        this.destinatario = destinatario;
-    }
-
-    public String getAssunto() {
-        return assunto;
-    }
-
-    public void setAssunto(String assunto) {
-        this.assunto = assunto;
-    }
-
-    public String getMensagem() {
-        return mensagem;
-    }
-
-    public void setMensagem(String mensagem) {
-        this.mensagem = mensagem;
-    }
     @Override
     public String toString() {
-        return "\nDe: "+remetente+"// Para: "+destinatario+"\n Assunto"+assunto+"\n"+mensagem+"\n.";
+        return "\nDe: "+sender+"// Para: "+to+"\nAssunto: "+tittle+"\nMenssagem: "+msg+"\n.";
     }
+
+    public String getTamanho() {
+        return tamanho+" KB's";
+    }
+
     
 }
-
